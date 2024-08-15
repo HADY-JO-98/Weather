a971a9d3cfdbe8e928a2752e6f4033d98f8f62c0
HADY-JO-98 <153452148+HADY-JO-98@users.noreply.github.com>
2024-08-15 08:29:23 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function checkweather(cityname) {
     var data = await response.json();
     console.log(data);
     if(data.message=='city not found'){
-        weath.innrHTML = "";
+        weath.innrHTML = "<img src='' alt=''>";
         document.getElementById("city").innerHTML = "";
         document.getElementById("temp").innerHTML = "";
         document.getElementById("numh").innerHTML = "";
