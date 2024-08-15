b675ef2aaeb3d132305967033489a20ca689fb75
HADY-JO-98 <153452148+HADY-JO-98@users.noreply.github.com>
2024-08-15 07:06:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -17,30 +17,36 @@ async function checkweather(cityname) {
     const response = await fetch(apiurl + cityname + '&appid=5d204ec4584d4eaaa366a27f75ab3e04');
     var data = await response.json();
     console.log(data);
-
-    document.getElementById("city").innerHTML = data.name;
-    document.getElementById("temp").innerHTML = Math.floor(data.main.temp) + "&#8451;";
-    document.getElementById("numh").innerHTML = data.main.humidity + "%";
-    document.getElementById("numw").innerHTML = data.wind.speed + "KM/H";
-
-    var weather = data.weather[0].main;
-
-    if (weather === "Clouds") {
-        weath.innerHTML = "<img src='images/clouds.png' alt='Cloudy'>";
-    } else if (weather === "Rain") {
-        weath.innerHTML = "<img src='images/rain.png' alt='Rainy'>";
-    } else if (weather === "Clear") {
-        weath.innerHTML = "<img src='images/sunny.png' alt='Sunny'>";
-    } else if (weather === "Snow") {
-        weath.innerHTML = "<img src='images/snow.png' alt='Snowy'>";
-    } else if (weather === "Thunderstorm") {
-        weath.innerHTML = "<img src='images/storm.png' alt='Thunderstorm'>";
-    } else {
-        weath.innerHTML = "<img src='images/partcloud.png' alt='Partly Cloudy'>";
+    if(data.message=='city not found'){
+        document.getElementById("city").innerHTML = " ";
+        document.getElementById("temp").innerHTML =  "&#8451;";
+        document.getElementById("numh").innerHTML =  "%";
+        document.getElementById("numw").innerHTML =  "KM/H";
+    }    
+    else{
+        document.getElementById("city").innerHTML = data.name;
+        document.getElementById("temp").innerHTML = Math.floor(data.main.temp) + "&#8451;";
+        document.getElementById("numh").innerHTML = data.main.humidity + "%";
+        document.getElementById("numw").innerHTML = data.wind.speed + "KM/H";
+    
+        var weather = data.weather[0].main;
+    
+        if (weather === "Clouds") {
+            weath.innerHTML = "<img src='images/clouds.png' alt='Cloudy'>";
+        } else if (weather === "Rain") {
+            weath.innerHTML = "<img src='images/rain.png' alt='Rainy'>";
+        } else if (weather === "Clear") {
+            weath.innerHTML = "<img src='images/sunny.png' alt='Sunny'>";
+        } else if (weather === "Snow") {
+            weath.innerHTML = "<img src='images/snow.png' alt='Snowy'>";
+        } else if (weather === "Thunderstorm") {
+            weath.innerHTML = "<img src='images/storm.png' alt='Thunderstorm'>";
+        } else {
+            weath.innerHTML = "<img src='images/partcloud.png' alt='Partly Cloudy'>";
+        }
+        cont.style.display="block";
     }
-    cont.style.display="block";
 }
-
 sbtn.addEventListener("click", () => {
     checkweather(searchbar.value);
 });
