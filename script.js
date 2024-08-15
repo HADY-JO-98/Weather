638f8a73ed2e4104dadedb303c832df3f497ff9f
HADY-JO-98 <153452148+HADY-JO-98@users.noreply.github.com>
2024-08-15 08:24:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,12 @@ async function checkweather(cityname) {
     var data = await response.json();
     console.log(data);
     if(data.message=='city not found'){
-        cont.style.display="none";
+        document.getElementById("city").innerHTML = "";
+        document.getElementById("temp").innerHTML = "";
+        document.getElementById("numh").innerHTML = "";
+        document.getElementById("numw").innerHTML = "";
+        windspeed.innerHTML = "";
+        humidity.innerHTML = "";
     }    
     else{
         cont.style.display="block";
